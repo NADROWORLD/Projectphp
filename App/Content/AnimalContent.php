5eb9e967141ec75e8674903153fff20f1c6fe057
CyrielleGl <cyrielle.gallou@gmail.com>
2021-06-07 16:07:43 +0200

    replace couleurPelage by couleur

--- App/Content/AnimalContent.php
+++ App/Content/AnimalContent.php
@@ -8,7 +8,7 @@ class AnimalContent {
             <img src='https://fakeimg.pl/286x180/?text=". $animal->getNom()."' class='card-img-top' alt='...'>
             <div class='card-body'>
                 <h5 class='card-title'>Nom: ".$animal->getNom()."</h5>
-                <h5 class='card-title'>Couleur: ".$animal->getCouleurPelage()."</h5>
+                <h5 class='card-title'>Couleur: ".$animal->getcouleur()."</h5>
                 <h5 class='card-title'>Age: ".$animal->getAge()."</h5>
                 <p class='card-text'>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
                 <a href='#' class='btn btn-primary'>Go somewhere</a>

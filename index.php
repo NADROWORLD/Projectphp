7812eac3a19b70b25f02b93e075da9fc2337d703
CyrielleGl <cyrielle.gallou@gmail.com>
2021-06-07 15:52:17 +0200

    better presentation

--- index.php
+++ index.php
@@ -6,16 +6,16 @@ require "App/Content/AnimalContent.php";
 
 echo "<br>";
 $chat1 = new Chat("Minou", "gris", 3, "Siamois");
-var_dump($chat1);
+// var_dump($chat1);
 echo "<br>";
 $chat2 = new Chat("Akira", "noir & blanc", 4, "Européen");
-var_dump($chat2);
+// var_dump($chat2);
 echo "<br>";
 $chien1 = new Chien("Pepper", "marron", 5, "Chihuahua");
-var_dump($chien1);
+// var_dump($chien1);
 echo "<br>";
 $chien2 = new Chien("Louna", "fauve charbonnée", 9, "Berger de Shetland");
-var_dump($chien2);
+// var_dump($chien2);
 echo "<br>";
 
 $createCard = new AnimalContent();

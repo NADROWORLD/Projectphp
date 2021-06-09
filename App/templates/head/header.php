e593ce6008d0b52cf959406658e8ac21cc212dcf
canelle delbreil <canellesunshine@hotmail.fr>
2021-06-09 19:24:42 +0200

    changing active class depending on the tab

--- App/templates/head/header.php
+++ App/templates/head/header.php
@@ -1,10 +1,33 @@
+<?php 
+
+switch ($_SERVER['REQUEST_URI']):
+    case ("/chats") :
+      $active = "chats";
+      break;
+    case ("/chiens") :
+      $active = "chiens";
+      break;
+    case ("/poissons") :
+      $active = "poissons";
+      break;
+    case ("/reptiles") :
+      $active = "reptiles";
+      break;
+    case ("/") :
+      $active = "home";
+      break;
+    default:
+      $active = "";
+      break;
+endswitch;
+?>
 <div class="navbar">
   <img class="logo" width=300px; src="./App/templates/img/logo.png" >
   <div style="margin-left: auto; margin-right: auto;">  
-  <a href="/chiens">Chiens</a>
-  <a href="/chats">Chats</a>
-  <a href="/poissons">Poissons</a>
-  <a href="/reptiles">Reptiles</a>
-  <a class="active" href="/">Accueil</a>
+  <a <?php if($active === "chiens"): ?> class="active" <?php endif ?> href="/chiens">Chiens</a>
+  <a <?php if($active === "chats"): ?> class="active" <?php endif ?> href="/chats">Chats</a>
+  <a <?php if($active === "poissons"): ?> class="active" <?php endif ?> href="/poissons">Poissons</a>
+  <a <?php if($active === "reptiles"): ?> class="active" <?php endif ?> href="/reptiles">Reptiles</a>
+  <a <?php if($active === "home"): ?> class="active" <?php endif ?> href="/">Accueil</a>
   </div>
 </div>

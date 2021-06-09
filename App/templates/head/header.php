70aee459de8d5853da0507a57a8cf64ef79b20dd
CyrielleGl <cyrielle.gallou@gmail.com>
2021-06-09 12:43:53 +0200

    navbar css

--- App/templates/head/header.php
+++ App/templates/head/header.php
@@ -1,8 +1,11 @@
 <div class="navbar">
   <img class="logo" width=300px; src="./App/templates/img/logo.png" >
-  
-  <a href="#Chiens">Chiens</a>
-  <a href="#Chats">Chats</a>
-  <a href="#Reptiles">Reptiles</a>
-  <a class="active" href="#home">Accueil</a>
+
+  <div style="margin-left: auto; margin-right: auto;">  
+    <a href="#Chiens">Chiens</a>
+    <a href="#Chats">Chats</a>
+    <a href="#Reptiles">Reptiles</a>
+    <a href="#Poissons">Poissons</a>
+    <a class="active" href="#home">Accueil</a>
+  </div>
 </div>

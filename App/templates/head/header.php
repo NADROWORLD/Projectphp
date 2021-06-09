3505bff36c6ad9e71917760e9564a84448c28cc4
CyrielleGl <cyrielle.gallou@gmail.com>
2021-06-09 12:55:03 +0200

    links

--- App/templates/head/header.php
+++ App/templates/head/header.php
@@ -2,10 +2,10 @@
   <img class="logo" width=300px; src="./App/templates/img/logo.png" style="z-index: 1">
 
   <div style="margin-left: auto; margin-right: auto; display: flex; z-index: 2">  
-    <a href="/chiens">Chiens</a>
-    <a href="/chats">Chats</a>
-    <a href="/reptiles">Reptiles</a>
-    <a href="/poissons">Poissons</a>
-    <a class="active" href="/">Accueil</a>
+    <a href="#chiens">Chiens</a>
+    <a href="#chats">Chats</a>
+    <a href="#reptiles">Reptiles</a>
+    <a href="#poissons">Poissons</a>
+    <a class="active" href="#home">Accueil</a>
   </div>
 </div>

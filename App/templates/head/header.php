4f7e057e68997a39345c3f91747fca689b69e679
canelle delbreil <canellesunshine@hotmail.fr>
2021-06-09 20:27:16 +0200

    changing home in nav-bar

--- App/templates/head/header.php
+++ App/templates/head/header.php
@@ -30,11 +30,11 @@ endswitch;
 <div class="navbar">
   <img class="logo" width=300px; src="/App/templates/img/logo.png" style="z-index: 1">
   <div style="margin-left: auto; margin-right: auto; display: flex; z-index: 2;">  
+  <a <?php if($active === "home"): ?> class="active" <?php endif ?> href="/">Accueil</a>
   <a <?php if($active === "chiens"): ?> class="active" <?php endif ?> href="/chiens">Chiens</a>
   <a <?php if($active === "chats"): ?> class="active" <?php endif ?> href="/chats">Chats</a>
   <a <?php if($active === "poissons"): ?> class="active" <?php endif ?> href="/poissons">Poissons</a>
   <a <?php if($active === "reptiles"): ?> class="active" <?php endif ?> href="/reptiles">Reptiles</a>
-  <a <?php if($active === "home"): ?> class="active" <?php endif ?> href="/">Accueil</a>
   <a <?php if($active === "register"): ?> class="active" <?php endif ?> href="/register">S'enregistrer</a>
   <a <?php if($active === "login"): ?> class="active" <?php endif ?> href="/login">Se connecter</a>
   </div>

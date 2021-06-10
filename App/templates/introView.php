8a8ecc58f87abea7ab59758bc3d6fe1ced29f551
CyrielleGl <cyrielle.gallou@gmail.com>
2021-06-10 16:37:23 +0200

    update intro

--- App/templates/introView.php
+++ App/templates/introView.php
@@ -3,10 +3,17 @@
 
     <div class="intro">
         <p>L'association Kira, c'est un ensemble de professionnels et de bénévoles passionnés, qui soignent les animaux que nous hébergeons. 
-        En attendant que nos protégés trouvent un nouveau foyer, notre équipe les éduque et les sociabilise.</p>
+        En attendant que nos protégés trouvent un nouveau foyer, notre équipe les éduque et les sociabilise.
+        </p>
         <p>Certains de nos animaux peuvent avoir vécu des traumatismes dans leurs vies passées, nos éducateurs sont là pour les aider à retrouver confiance en l'humain.
-        Si vous êtes amenés à adopter un de nos compagnons blessés, vous serez suivi dans les premières semaines afin de s'assurer que l'adaptation de l'animal se passe bien.</p>
-        <p></p>
+        Si vous êtes amenés à adopter un de nos compagnons blessés psychologiquement, vous serez suivi dans les premières semaines afin de s'assurer que l'adaptation de l'animal se passe bien.
+        </p>
+        <p>Vous pouvez parcourir notre site pour prendre connaissance des animaux que nous avons pour adoption en ce moment. 
+        Si vous avez un coup de coeur sur un animal, vous pouvez faire une demande de réservation en cliquant sur le bouton "réservez-moi" présent la fiche de chaque animal.
+        Votre mail et votre numéro de téléphone vous seront demandés afin que nous puissions vous recontacter pour échanger à propos de vous et de l'animal que vous souhaitez adopter.
+        Puis nous prendrons rendez-vous afin de vous rencontrer et que vous rencontriez l'animal au sein de notre association.
+        Dans certains cas, nous nous réservons le droit de venir vérifier votre logement (notamment pour l'adoption de chiens qui ne sauraient vivre en appartement), afin d'éviter un nouvel abandon de l'animal.
+        </p>
     </div>
 
   <?php require('./App/templates/head/footer.php'); ?>

e97f34617ac33fb30133ed2caee9b78db6ba5458
CyrielleGl <cyrielle.gallou@gmail.com>
2021-06-10 17:03:55 +0200

    css buttons

--- App/templates/loginView.php
+++ App/templates/loginView.php
@@ -7,7 +7,7 @@
                 <form class="logform" action="login" id="myform" method="POST" enctype="multipart/form-data">
                     <input name="email" placeholder="Adresse Mail" type="text" value="" size="30"/>
                     <input name="password" placeholder="Mot de Passe" type="password" value="" size="30"/>
-                    <input name="bouton" type="submit" id="seconnecter" value="Connexion"
+                    <input class="btn-primary" name="bouton" type="submit" id="seconnecter" value="Connexion"
                       onclick="document.forms['myform'].submit();"/>
                 </form>
               </div>

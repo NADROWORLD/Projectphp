11ebc6959efc5a59242cb20464787548492e5027
Hugo Fief <hfief1806@gmail.com>
2021-06-11 11:52:12 +0200

    add error message book

--- App/templates/animalProfileView.php
+++ App/templates/animalProfileView.php
@@ -1,12 +1,24 @@
 <div class="profile-content">
   <?php if (isset($_SESSION['user'])) { ?> 
     <button class="btn btn-primary" onclick="bookAnimal()">Réverver</button>
+  <?php } else if (!isset($_SESSION['user'])) { ?>
+    <button class="btn btn-primary" onclick="bookError()">Réverver</button>
   <?php } ?>
 
   <?php require('./App/templates/head/footer.php'); ?>
 </div>
 
 <script>
+  function bookError(){
+    Swal.fire({
+      title: "Erreur",
+      icon: "error",
+      text: "Vous devez vous connecter",
+    }).then(function () {
+      window.location.href = "../login";
+    });  
+  }
+
   function bookAnimal(){
     Swal.fire({
       title: "Succès!",
